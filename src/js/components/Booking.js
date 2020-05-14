155d6eff3c82049a3c27cb686892c54e0c8732c4
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-05-14 21:54:16 +0200

    Add HourPicker.js class in submodule 10.3

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import {select, templates} from '/js/settings.js'; 
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(bookingWidget) {
@@ -17,6 +18,7 @@ class Booking {
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount); //we właściwości thisBooking.dom.peopleAmount zapisywać pojedynczy element znaleziony we wrapperze i pasujący do selektora select.booking.peopleAmount.
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount); //analogicznie do peopleAmount znaleźć i zapisać element dla hoursAmount.
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
  
   initWidgets(){ //we właściwościach thisBooking.peopleAmount i thisBooking.hoursAmount zapisywać nowe instancje klasy AmountWidget, którym jako argument przekazujemy odpowiednie właściwości z obiektu thisBooking.dom.
@@ -24,6 +26,7 @@ class Booking {
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   } 
 }
 
---
+++ src/js/components/HourPicker.js
@@ -0,0 +1,38 @@
+/* global rangeSlider */
+import {select, settings} from '/js/settings.js';
+import {utils} from '/js/utils.js';
+import BaseWidget from './BaseWidget.js';
+
+class HourPicker extends BaseWidget{ // HP jest rozszerzeniem klasy BaseWidget.
+  constructor(wrapper){
+    super(wrapper, settings.hours.open); 
+    const thisWidget = this;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+    thisWidget.initPlugin();
+    thisWidget.value = thisWidget.dom.input.value;
+  }
+  initPlugin(){
+    const thisWidget = this;
+    rangeSlider.create(thisWidget.dom.input); //uruchomienie pluginu.
+    thisWidget.dom.input.addEventListener('input', function(){    
+      thisWidget.value = thisWidget.dom.input.value;
+      console.log('thisWidget.dom.input.value:', thisWidget.dom.input.value);
+    });
+  }
+  parseValue(){ 
+    const thisWidget = this;
+    const result = utils.numberToHour(thisWidget.dom.input.value);//zamienia liczby na zapis godzinowy, czyli np. 12 na '12:00'.
+    return result;
+  }
+
+  isValid(){ //nie może zostać domyślna z BaseWidget.
+    return true; //nie mamy dla niej zastosowania w tym widgecie, ponieważ plugin zajmie się dbaniem o poprawność danych, więc niech po prostu zwraca true.
+  }
+  renderValue(){ //ma zamieniać zawartość elementu thisWidget.dom.output na wartość widgetu.
+    const thisWidget = this;
+    thisWidget.dom.output = thisWidget.value;
+  }
+}
+
+export default HourPicker;
